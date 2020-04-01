d510fd1a62d3e9c5da4c8dd85bca5912bd60748b
Seono <pianist5555@gmail.com>
2020-04-01 14:12:04 +0900

    0401

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './modules';
+import loggerMiddleware from './lib/loggerMiddleware.js'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/lib/loggerMiddleware.js
+++ src/lib/loggerMiddleware.js
@@ -1,5 +1,18 @@
 const loggerMiddleware = store => next => action => {
-    // 대기
+    console.group(action && action.type);
+    console.log('이전 상태', store.getState());
+    console.log('액션', action);
+    next(action) // 다음 미들웨어 or 리듀서에게 전달
+    console.log('다음 상태', store.getState()); // 액션이 전달 되었으니 업데이트된 상태
+    console.groupEnd();
 }
+
+/*const loggerMiddleware = function loggerMiddleware(store) {
+    return function(next) {
+        return function(action) {
+
+        }
+    }
+}*/
 
 export default loggerMiddleware;
